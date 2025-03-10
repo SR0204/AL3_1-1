914bcadaac1511fe79e7d9bab01bda0f01a6d80a
SR0204 <k023g0023@g.neec.ac.jp>
2025-03-10 14:49:30 +0900

    a

--- DirectXGame/MathUtilityForText.h
+++ DirectXGame/MathUtilityForText.h
@@ -16,7 +16,7 @@ float EaseInOut(float x1, float x2, float t);
 
 float Lerp(float x1, float x2, float t);
 
-Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);
+Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);	
 
 //2項演算子のオーバーロード
 //Vector3の足し算
